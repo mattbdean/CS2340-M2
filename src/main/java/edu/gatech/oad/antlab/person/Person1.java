bf018a9aac65d0b9ed923a9b49d88d328ae2c92a
jlefelhocz <jlefelhocz@yahoo.com>
2018-01-23 11:31:56 -0500

    Edited Person1 file to finish method

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -3,8 +3,8 @@ package edu.gatech.oad.antlab.person;
 /**
  *  A simple class for person 1
  *  returns their name and a
- *  modified string 
- *  
+ *  modified string
+ *
  *  @author Bob
  *  @version 1.1
  */
@@ -30,20 +30,36 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
+      char[] name = input.toCharArray();
+      String val = "";
+      if (name.length >= 2) {
+        char first = name[0];
+        char second = name[1];
+        for (int i = 2; i < name.length; i++) {
+          name[i - 2] = name[i];
+        }
+        name[name.length - 2] = first;
+        name[name.length - 1] = second;
+        for (int i = 0; i < name.length; i++) {
+          val += name[i];
+        }
+        return val;
+    } else if (name.length == 1) {
+        return input;
+    }
 	  //Person 1 put your implementation here
 	  return null;
 	}
-	
+
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
 	  return name + calc(input);
 	}
-
 }
